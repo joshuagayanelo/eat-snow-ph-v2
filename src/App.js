38543a47b5297cc3537d9906bebc9888df002268
joshuagayanelo <iamjoshuagayanelo@gmail.com>
2023-07-10 20:13:48 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         This is me learning react!
         </a>
       </header>
     </div>
